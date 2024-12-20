ec6fdd93c82ae5f63ccc539ca0d4f17f833d63d5
Tobyran <53066925+Tobyran@users.noreply.github.com>
2024-12-19 23:32:12 -0500

    Added enemy manager

---
+++ Assets/Scripts/Enemy system/EnemySystemManager.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySystemManager : MonoBehaviour
+{
+    [SerializeField] private GameObject enemyPrefab;
+    [SerializeField] private int numberOfEnemies;
+    [SerializeField] private List<GameObject> enemyList;
+    [SerializeField] private Transform[] positions;
+    [SerializeField] private Transform enemyPool;
+    [SerializeField] private float timeBetweenEnemies;
+    [SerializeField] private float enemyOffset;
+
+    private void Start()
+    {
+        AddEnemyToPool(numberOfEnemies);
+        StartCoroutine(SpawnEnemiesCoroutine());
+    }
+
+    private void AddEnemyToPool(int amount)
+    {
+        for (int i = 0; i < amount; i++)
+        {
+            GameObject stump = Instantiate(enemyPrefab);
+            stump.SetActive(false);
+            enemyList.Add(stump);
+            stump.transform.parent = enemyPool;
+        }
+    }
+
+    public GameObject RequestEnemy()
+    {
+        for (int i = 0; i < enemyList.Count; i++)
+        {
+            if (!enemyList[i].activeSelf)
+            {
+                enemyList[i].SetActive(true);
+                return enemyList[i];
+            }
+        }
+        AddEnemyToPool(1);
+        enemyList[enemyList.Count - 1].SetActive(true);
+        return enemyList[enemyList.Count - 1];
+    }
+
+    private IEnumerator SpawnEnemiesCoroutine()
+    {
+        while (true) 
+        {
+            SpawnEnemy();
+            yield return new WaitForSeconds(timeBetweenEnemies);
+        }
+    }
+
+    private void SpawnEnemy()
+    {
+        GameObject enemy = RequestEnemy();
+
+        int randomInt = Random.Range(0, positions.Length);
+        enemy.transform.position = positions[randomInt].position + Vector3.left * enemyOffset;
+    }
+}
